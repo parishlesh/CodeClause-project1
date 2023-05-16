830b7ce92211339e7975022ea4fd0741a15ac1ed
Parishlesh <122471748+parishlesh@users.noreply.github.com>
2023-05-16 21:20:02 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,28 @@
+var input= document.getElementById('input')
+var output= document.getElementById('output')
+var inputType= document.getElementById('inputType')
+var outputType= document.getElementById('outputType')
+var inputTypeValue, outputTypeValue
+
+
+const convert_btn= document.getElementById('convertButton')
+
+
+convert_btn.addEventListener("click", myOutput)
+inputTypeValue = inputType.value
+outputTypeValue = outputType.value
+
+function myOutput(){
+     
+    if(inputTypeValue === 'inch' && outputTypeValue === 'feet')
+    {alert("ok")
+        output.value=(input.value)/12;
+        document.getElementById('output')=output.value
+        console.log("123")
+    }
+    else if(inputTypeValue === 'inch' && outputTypeValue === 'meter')
+    {
+        output.value=(input.value)/39.37;
+    }
+}
+
