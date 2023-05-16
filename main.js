58de41137171cca497fdbd4e1138a69c3121a1f3
Parishlesh <122471748+parishlesh@users.noreply.github.com>
2023-05-16 22:10:19 +0530

    final commit

--- main.js
+++ main.js
@@ -2,27 +2,114 @@ var input= document.getElementById('input')
 var output= document.getElementById('output')
 var inputType= document.getElementById('inputType')
 var outputType= document.getElementById('outputType')
-var inputTypeValue, outputTypeValue
+
 
 
 const convert_btn= document.getElementById('convertButton')
 
 
 convert_btn.addEventListener("click", myOutput)
-// inputTypeValue = inputType.value
-// outputTypeValue = outputType.value
 
 function myOutput(){
      
-    if(inputType.value === 'inch' && outputTypeValue === 'feet')
-    {alert("ok")
-        output.value=(input.value)/12;
-        document.getElementById('output')=output.value
-        console.log("123")
-    }
-    else if(inputTypeValue === 'inch' && outputTypeValue === 'meter')
+    if(inputType.value === 'inch' && outputType.value === 'feet')
     {
-        output.value=(input.value)/39.37;
+        result=(input.value)/12;
+        document.getElementById('output').innerHTML=result
+    }
+    else if(inputType.value === 'inch' && outputType.value === 'km')
+    {
+        result=(input.value)/39370;
+        document.getElementById('output').innerHTML=result
+    }
+    else if(inputType.value === 'inch' && outputType.value === 'meter')
+    {
+        result=(input.value)/39.37;
+        document.getElementById('output').innerHTML=result
+    }
+    else if(inputType.value === 'inch' && outputType.value === 'centimeter')
+    {
+        result=(input.value)*2.54;
+        document.getElementById('output').innerHTML=result
+    }
+    else if(inputType.value === 'km' && outputType.value === 'inch')
+    {
+        result=(input.value)*39370;
+        document.getElementById('output').innerHTML=result
+    }
+    else if(inputType.value === 'km' && outputType.value === 'meter')
+    {
+        result=(input.value)*1000
+        document.getElementById('output').innerHTML=result
+    }
+    else if(inputType.value === 'km' && outputType.value === 'centimeter')
+    {
+        result=(input.value)*100000;
+        document.getElementById('output').innerHTML=result
+    }
+    else if(inputType.value === 'km' && outputType.value === 'feet')
+    {
+        result=(input.value)*3281;
+        document.getElementById('output').innerHTML=result
+    }
+    else if(inputType.value === 'meter' && outputType.value === 'inch')
+    {
+        result=(input.value)*39.37;
+        document.getElementById('output').innerHTML=result
+    }
+    else if(inputType.value === 'meter' && outputType.value === 'km')
+    {
+        result=(input.value)/1000;
+        document.getElementById('output').innerHTML=result
+    }
+    else if(inputType.value === 'meter' && outputType.value === 'centimeter')
+    {
+        result=(input.value)*100;
+        document.getElementById('output').innerHTML=result
+    }
+    else if(inputType.value === 'meter' && outputType.value === 'feet')
+    {
+        result=(input.value)*3.281;
+        document.getElementById('output').innerHTML=result
+    }
+    else if(inputType.value === 'centimeter' && outputType.value === 'inch')
+    {
+        result=(input.value)/2.54;
+        document.getElementById('output').innerHTML=result
+    }
+    else if(inputType.value === 'centimeter' && outputType.value === 'km')
+    {
+        result=(input.value)/100000;
+        document.getElementById('output').innerHTML=result
+    }
+    else if(inputType.value === 'centimeter' && outputType.value === 'meter')
+    {
+        result=(input.value)/100;
+        document.getElementById('output').innerHTML=result
+    }
+    else if(inputType.value === 'centimeter' && outputType.value === 'feet')
+    {
+        result=(input.value)/30.48;
+        document.getElementById('output').innerHTML=result
+    }
+    else if(inputType.value === 'feet' && outputType.value === 'inch')
+    {
+        result=(input.value)*12;
+        document.getElementById('output').innerHTML=result
+    }
+    else if(inputType.value === 'feet' && outputType.value === 'km')
+    {
+        result=(input.value)/3281;
+        document.getElementById('output').innerHTML=result
+    }
+    else if(inputType.value === 'feet' && outputType.value === 'meter')
+    {
+        result=(input.value)/3.281;
+        document.getElementById('output').innerHTML=result
+    }
+    else if(inputType.value === 'feet' && outputType.value === 'centimeter')
+    {
+        result=(input.value)*30.48;
+        document.getElementById('output').innerHTML=result
     }
 }
-
